b378535ef5d9ae78cd5cc3341c9de40d850f97c2
Cory <Corydixon1@gmail.com>
2023-07-31 20:39:41 -0400

    My Name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>My Portfolio</h1>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>My Portfolio</h1>
+			<h2>Cory Dixon</h2>
+		</div>
+	);
 }
 
 export default App;
