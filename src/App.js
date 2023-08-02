5eccc0fe07c707be7cff4c7e2a515327c956fc5d
Cory <Corydixon1@gmail.com>
2023-08-02 18:07:20 -0400

    about me

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>My Portfolio</h1>
-			<h2>Cory Dixon</h2>
+			<h2>
+				Hi, my name is Cory Dixon and I am a full-stack web developer based in
+				Kitchener-Waterloo, Ontario, Canada
+			</h2>
+			<h3>My coding skills are in:</h3>
+			<h2>About Me</h2>
+			<h3>
+				I'm a family man, that enjoys music, sports and all things tech. Being a
+				web dev is intriguing to me because you are always learning to be at the
+				top of your game.{' '}
+			</h3>
 		</div>
 	);
 }
